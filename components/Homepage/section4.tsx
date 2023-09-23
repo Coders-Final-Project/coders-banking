73cd4ef02b54e1feb0d2c1da9acfb639922f5552
Allahverdi <isaevallahverdi@gmail.com>
2023-09-24 01:27:25 +0400

    features/homepage

---
+++ components/Homepage/section4.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "@/sass/components/_section4.scss";
+
+const Section4 = () => {
+  return (
+    <div className="section4__container">
+      <p className="section4__container--title">
+        From Offer Letter to Onboarded in <span>Minutes</span>
+      </p>
+      <p className="section4__container--desc">
+        Create a custom contract that’s compliant with local laws, send it to
+        sign, and they'll get onboarded almost instantly.
+      </p>
+      <div className="section4__container--img"></div>
+    </div>
+  );
+};
+
+export default Section4;
---
+++ components/Homepage/section6.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "@/sass/components/_section6.scss";
+
+const Section6 = () => {
+  return (
+    <div className="section6__container">
+      <div className="section6__container--title">
+        <p>
+          Send Payment <span>Faster</span>
+        </p>
+        <p>
+          We’ll keep you and your hires compliant with ever-changing taxes and
+          regulations.
+        </p>
+      </div>
+      <div className="section6__container--img"></div>
+    </div>
+  );
+};
+
+export default Section6;
